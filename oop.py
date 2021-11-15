1287373fa58f2cceaf06cb5da88fc58726e27991
abi wardani <abiwardani@abis-MacBook-Pro.local>
2021-11-15 16:37:07 +0700

    property,setter,private variables

--- oop.py
+++ oop.py
@@ -6,22 +6,38 @@ class User: # class name
     def __init__(self, name, email, role): 
         self.name = name #attributes
         self.email = email
-        self.role = role
+        self.__role = role
         User.total += 1
 
     # method
     def biodata(self):
-        return f'{self.name} {self.email} {self.role}'
+        return f'{self.name} {self.email} {self.__role}'
 
     def update_name(self, new_name):
-        if self.role == 'admin':
+        if self.__role == 'admin':
             self.name = new_name
+    #property
+    @property
+    def role(self):
+        return self.__role
+
+    #setter
+    @role.setter
+    def role(self, new_role):
+        if new_role == 'admin':
+            self.__role = new_role
+        else:
+            print('Role must be admin')
+    # def getRoles(self):
+    #     return self.__role
 
 # abi -> instance variables
 abi = User('Abi', 'abi@gmail.com', 'admin')
 print(abi.biodata())
 abi.update_name('dani')
 print(abi.biodata())
+print(abi.__dict__)
+# print(abi.getRoles())
 print(User.total)
 
 print("===========")
@@ -29,8 +45,13 @@ print("===========")
 xian = User('Xian', 'xian@gmail.com', 'user')
 print(xian.biodata())
 xian.update_name('chris')
-print(xian.biodata())
+print(xian.biodata()) #nama tidak berubah
+print(xian.__dict__)
+xian.role = 'admin'
+print(xian.biodata()) #role berubah
 print(User.total)
+
+# print(xian.__role) #output error
 
 
 # print(abi.name)
