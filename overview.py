f9c5df24e01d58ee35b0e3122220818f312d2e7b
abi wardani <abiwardani@abis-MacBook-Pro.local>
2021-11-17 11:15:12 +0700

    add overview

---
+++ overview.py
@@ -0,0 +1,8 @@
+# class NameOfClass():
+#     def __init__(self,param1,param2):
+#         self.param1 = param1
+#         self.param2 = param2
+    
+#     def method(self):
+#         #perform some action
+#         print(f'{self.param1} {self.param2}')
